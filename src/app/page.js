2bf259f87fbb61cf667b9187d99cd18d5901881f
Heejin Kim <gmlwls768@naver.com>
2025-05-22 06:34:53 +0900

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -158,7 +158,6 @@ export default function Portfolio() {
                     "MySQL",
                     "Redis",
                     "Docker",
-                    "Github Actions",
                     "OAuth 2.0",
                     "JWT",
                   ]}
